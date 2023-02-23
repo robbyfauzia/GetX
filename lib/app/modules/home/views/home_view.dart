d642f1664becea877ff2bb0ab342edd88ee910c7
robbyfauzia <robbyfauzia123@gmail.com>
2023-02-23 10:11:24 +0700

    Mobile

---
+++ lib/app/modules/home/views/home_view.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:hexcolor/hexcolor.dart';
+import 'package:lottie/lottie.dart';
+
+import '../controllers/home_controller.dart';
+
+class HomeView extends GetView<HomeController> {
+  const HomeView({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: HexColor('#feeee8'),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Lottie.network(
+              'https://gist.githubusercontent.com/olipiskandar/2095343e6b34255dcfb042166c4a3283/raw/d76e1121a2124640481edcf6e7712130304d6236/praujikom_kucing.json',
+              fit: BoxFit.cover,
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            Text(
+              'Aplikasi Portal Berita Indonesia',
+              style: GoogleFonts.roboto(
+                textStyle: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 30,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+  // Widget build(BuildContext context) {
+  //   return Scaffold(
+  //     appBar: AppBar(
+  //       title: const Text('HomeView'),
+  //       centerTitle: true,
+  //     ),
+  //     body: Center(
+  //       child: Text(
+  //         'HomeView is working',
+  //         style: TextStyle(fontSize: 20),
+  //       ),
+  //     ),
+  //   );
+  // }
+}
