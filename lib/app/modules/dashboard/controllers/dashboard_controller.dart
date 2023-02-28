2586caa81b986ab83686d5bd47dec8893deee1b3
robbyfauzia <robbyfauzia123@gmail.com>
2023-02-28 12:30:37 +0700

    Profile

--- lib/app/modules/dashboard/controllers/dashboard_controller.dart
+++ lib/app/modules/dashboard/controllers/dashboard_controller.dart
@@ -1,7 +1,10 @@
 import 'dart:convert';
 
 import 'package:get/get.dart';
+import 'package:project/app/data/entertainment_response.dart';
 import 'package:project/app/data/headline_response.dart';
+import 'package:project/app/data/sports_response.dart';
+import 'package:project/app/data/technology_response.dart';
 import 'package:project/app/utils/api.dart';
 
 class DashboardController extends GetxController {
@@ -31,6 +34,24 @@ class DashboardController extends GetxController {
     final response = await _getConnect.get(BaseUrl.headline);
     //mengembalikan data response dalam bentuk HeadlineResponse setelah di-decode dari JSON
     return HeadlineResponse.fromJson(jsonDecode(response.body));
+  }
+  Future<TechnologyResponse> getTechnology() async {
+    //memanggil fungsi getConnect untuk melakukan request ke BaseUrl.headline
+    final response = await _getConnect.get(BaseUrl.technology);
+    //mengembalikan data response dalam bentuk TechnologyResponse setelah di-decode dari JSON
+    return TechnologyResponse.fromJson(jsonDecode(response.body));
+  }
+  Future<SportsResponse> getSports() async {
+    //memanggil fungsi getConnect untuk melakukan request ke BaseUrl.headline
+    final response = await _getConnect.get(BaseUrl.sports);
+    //mengembalikan data response dalam bentuk SportsResponse setelah di-decode dari JSON
+    return SportsResponse.fromJson(jsonDecode(response.body));
+  }
+  Future<EntertainmentResponse> getEntertainment() async {
+    //memanggil fungsi getConnect untuk melakukan request ke BaseUrl.headline
+    final response = await _getConnect.get(BaseUrl.entertainment);
+    //mengembalikan data response dalam bentuk EntertainmentResponse setelah di-decode dari JSON
+    return EntertainmentResponse.fromJson(jsonDecode(response.body));
   }
 }
 
--- lib/app/modules/dashboard/views/dashboard_view.dart
+++ lib/app/modules/dashboard/views/dashboard_view.dart
@@ -3,6 +3,9 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:lottie/lottie.dart';
+import 'package:project/app/data/entertainment_response.dart';
+import 'package:project/app/data/sports_response.dart';
+import 'package:project/app/data/technology_response.dart';
 import 'package:project/app/modules/home/views/home_view.dart';
 
 import '../../../data/headline_response.dart';
@@ -19,7 +22,7 @@ class DashboardView extends GetView<DashboardController> {
   return SafeArea(
     // Widget SafeArea menempatkan semua konten widget ke dalam area yang aman (safe area) dari layar.
     child: DefaultTabController(
-      length: 4,
+      length: 5,
       // Widget DefaultTabController digunakan untuk mengatur tab di aplikasi.
       child: Scaffold(
         floatingActionButton: FloatingActionButton(
@@ -82,6 +85,7 @@ class DashboardView extends GetView<DashboardController> {
                     Tab(text: "Teknologi"),
                     Tab(text: "Olahraga"),
                     Tab(text: "Hiburan"),
+                    Tab(text: "Profile"),
                   ],
                 ),
               ),
@@ -93,8 +97,10 @@ class DashboardView extends GetView<DashboardController> {
           children: [
             // Properti children digunakan untuk menentukan konten yang akan ditampilkan pada masing-masing tab.
             headline(controller, scrollController),
-            Center(child: Text('Berita Teknologi')),
-            Center(child: Text('Berita Sains')),
+            technology(controller, scrollController),
+            sports(controller, scrollController),
+            entertainment(controller, scrollController),
+            profile(),
           ],
         ),
       ),
@@ -188,4 +194,346 @@ FutureBuilder<HeadlineResponse> headline(DashboardController controller, ScrollC
     },
   );
 }
+FutureBuilder<TechnologyResponse> technology(DashboardController controller, ScrollController scrollController) {
+  return FutureBuilder<TechnologyResponse>(
+    // Mendapatkan future data headline dari controller
+    future: controller.getTechnology(),
+    builder: (context, snapshot) {
+      // Jika koneksi masih dalam keadaan waiting/tunggu, tampilkan widget Lottie loading
+      if (snapshot.connectionState == ConnectionState.waiting) {
+        return Center(
+          child: Lottie.network(
+            // Menggunakan animasi Lottie untuk tampilan loading
+            'https://gist.githubusercontent.com/olipiskandar/4f08ac098c81c32ebc02c55f5b11127b/raw/6e21dc500323da795e8b61b5558748b5c7885157/loading.json',
+            repeat: true,
+            width: MediaQuery.of(context).size.width / 1,
+          ),
+        );
+      }
+      // Jika tidak ada data yang diterima, tampilkan pesan "Tidak ada data"
+      if (!snapshot.hasData) {
+        return const Center(child: Text("Tidak ada data"));
+      }
+
+      // Jika data diterima, tampilkan daftar headline dalam bentuk ListView.Builder
+      return ListView.builder(
+        itemCount: snapshot.data!.data!.length,
+        controller: scrollController,
+        shrinkWrap: true,
+        itemBuilder: (context, index) {
+          // Tampilan untuk setiap item headline dalam ListView.Builder
+          return Container(
+            padding: const EdgeInsets.only(
+              top: 5,
+              left: 8,
+              right: 8,
+              bottom: 5,
+            ),
+            height: 110,
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                // Widget untuk menampilkan gambar headline dengan menggunakan url gambar dari data yang diterima
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(8.0),
+                  child: Image.network(
+                    snapshot.data!.data![index].urlToImage.toString(),
+                    height: 130,
+                    width: 130,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                const SizedBox(
+                  width: 10,
+                ),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      // Widget untuk menampilkan judul headline dengan menggunakan data yang diterima
+                      Text(
+                        snapshot.data!.data![index].title.toString(),
+                        overflow: TextOverflow.ellipsis,
+                        maxLines: 2,
+                      ),
+                      const SizedBox(
+                        height: 2,
+                      ),
+                      // Widget untuk menampilkan informasi author dan sumber headline dengan menggunakan data yang diterima
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text('Author : ${snapshot.data!.data![index].author}'),
+                          Text('Sumber :${snapshot.data!.data![index].name}'),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          );
+        },
+      );
+    },
+  );
+}
+FutureBuilder<SportsResponse> sports(DashboardController controller, ScrollController scrollController) {
+  return FutureBuilder<SportsResponse>(
+    // Mendapatkan future data headline dari controller
+    future: controller.getSports(),
+    builder: (context, snapshot) {
+      // Jika koneksi masih dalam keadaan waiting/tunggu, tampilkan widget Lottie loading
+      if (snapshot.connectionState == ConnectionState.waiting) {
+        return Center(
+          child: Lottie.network(
+            // Menggunakan animasi Lottie untuk tampilan loading
+            'https://gist.githubusercontent.com/olipiskandar/4f08ac098c81c32ebc02c55f5b11127b/raw/6e21dc500323da795e8b61b5558748b5c7885157/loading.json',
+            repeat: true,
+            width: MediaQuery.of(context).size.width / 1,
+          ),
+        );
+      }
+      // Jika tidak ada data yang diterima, tampilkan pesan "Tidak ada data"
+      if (!snapshot.hasData) {
+        return const Center(child: Text("Tidak ada data"));
+      }
+
+      // Jika data diterima, tampilkan daftar headline dalam bentuk ListView.Builder
+      return ListView.builder(
+        itemCount: snapshot.data!.data!.length,
+        controller: scrollController,
+        shrinkWrap: true,
+        itemBuilder: (context, index) {
+          // Tampilan untuk setiap item headline dalam ListView.Builder
+          return Container(
+            padding: const EdgeInsets.only(
+              top: 5,
+              left: 8,
+              right: 8,
+              bottom: 5,
+            ),
+            height: 110,
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                // Widget untuk menampilkan gambar headline dengan menggunakan url gambar dari data yang diterima
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(8.0),
+                  child: Image.network(
+                    snapshot.data!.data![index].urlToImage.toString(),
+                    height: 130,
+                    width: 130,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                const SizedBox(
+                  width: 10,
+                ),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      // Widget untuk menampilkan judul headline dengan menggunakan data yang diterima
+                      Text(
+                        snapshot.data!.data![index].title.toString(),
+                        overflow: TextOverflow.ellipsis,
+                        maxLines: 2,
+                      ),
+                      const SizedBox(
+                        height: 2,
+                      ),
+                      // Widget untuk menampilkan informasi author dan sumber headline dengan menggunakan data yang diterima
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text('Author : ${snapshot.data!.data![index].author}'),
+                          Text('Sumber :${snapshot.data!.data![index].name}'),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          );
+        },
+      );
+    },
+  );
+}
+FutureBuilder<EntertainmentResponse> entertainment(DashboardController controller, ScrollController scrollController) {
+  return FutureBuilder<EntertainmentResponse>(
+    // Mendapatkan future data headline dari controller
+    future: controller.getEntertainment(),
+    builder: (context, snapshot) {
+      // Jika koneksi masih dalam keadaan waiting/tunggu, tampilkan widget Lottie loading
+      if (snapshot.connectionState == ConnectionState.waiting) {
+        return Center(
+          child: Lottie.network(
+            // Menggunakan animasi Lottie untuk tampilan loading
+            'https://gist.githubusercontent.com/olipiskandar/4f08ac098c81c32ebc02c55f5b11127b/raw/6e21dc500323da795e8b61b5558748b5c7885157/loading.json',
+            repeat: true,
+            width: MediaQuery.of(context).size.width / 1,
+          ),
+        );
+      }
+      // Jika tidak ada data yang diterima, tampilkan pesan "Tidak ada data"
+      if (!snapshot.hasData) {
+        return const Center(child: Text("Tidak ada data"));
+      }
+
+      // Jika data diterima, tampilkan daftar headline dalam bentuk ListView.Builder
+      return ListView.builder(
+        itemCount: snapshot.data!.data!.length,
+        controller: scrollController,
+        shrinkWrap: true,
+        itemBuilder: (context, index) {
+          // Tampilan untuk setiap item headline dalam ListView.Builder
+          return Container(
+            padding: const EdgeInsets.only(
+              top: 5,
+              left: 8,
+              right: 8,
+              bottom: 5,
+            ),
+            height: 110,
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                // Widget untuk menampilkan gambar headline dengan menggunakan url gambar dari data yang diterima
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(8.0),
+                  child: Image.network(
+                    snapshot.data!.data![index].urlToImage.toString(),
+                    height: 130,
+                    width: 130,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                const SizedBox(
+                  width: 10,
+                ),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      // Widget untuk menampilkan judul headline dengan menggunakan data yang diterima
+                      Text(
+                        snapshot.data!.data![index].title.toString(),
+                        overflow: TextOverflow.ellipsis,
+                        maxLines: 2,
+                      ),
+                      const SizedBox(
+                        height: 2,
+                      ),
+                      // Widget untuk menampilkan informasi author dan sumber headline dengan menggunakan data yang diterima
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text('Author : ${snapshot.data!.data![index].author}'),
+                          Text('Sumber :${snapshot.data!.data![index].name}'),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          );
+        },
+      );
+    },
+  );
+}
+profile() {
+    final auth = GetStorage();
+    return Scaffold(
+        body: ListView(physics: BouncingScrollPhysics(), children: [
+      Container(
+          padding: EdgeInsets.symmetric(horizontal: 48),
+          child:
+              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                ClipOval(
+                  child: Image.asset(
+                    'assets/img/robby.jpg',
+                    height: 100,
+                    width: 100,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 20),
+            Text(
+              'Nama',
+            ),
+            const SizedBox(height: 10),
+            Text(
+              auth.read('full_name').toString(),
+            ),
+            // const SizedBox(height: 16),
+            Divider(),
+            Text(
+              'Email',
+            ),
+            const SizedBox(height: 10),
+            Text('robby_043@smkassalaambandung.sch.id'),
+            // auth.read('email'),
+            Divider(),
+            Text(
+              'Deskripsi',
+            ),
+            const SizedBox(height: 10),
+            Text(
+                'Halo Saya Robby Shelby. Saya Tinggal Dibandung, Saat Ini Saya Bersekolah Di Smk Assalaam Bandung, Dengan Jurusan Rekayasa Perangkat Lunak'),
+            Divider(),
+          ])),
+      const SizedBox(height: 100),
+      Center(child: Text('Ikuti Saya')),
+      SizedBox(
+        height: 20,
+      ),
+      Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ClipOval(
+                child: Image.network(
+                  'https://cdn-icons-png.flaticon.com/512/25/25231.png',
+                  height: 40,
+                  width: 40,
+                  fit: BoxFit.cover,
+                ),
+              ),
+              ClipOval(
+                child: Image.network(
+                  'https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Facebook_f_logo_%282019%29.svg/2048px-Facebook_f_logo_%282019%29.svg.png',
+                  height: 40,
+                  width: 40,
+                  fit: BoxFit.cover,
+                ),
+              ),
+              ClipOval(
+                child: Image.network(
+                  'https://png.pngtree.com/png-vector/20221018/ourmid/pngtree-instagram-icon-png-image_6315974.png',
+                  height: 40,
+                  width: 40,
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ],
+          ),
+        ],
+      )
+    ]));
+  }
 }
